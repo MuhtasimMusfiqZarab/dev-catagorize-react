3a30a943d777e35facd8d4a0d42998e957fb3ac3
zarab <musfiqzarab@iut-dhaka.edu>
2019-05-18 16:21:21 +0600

    making minor changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Product = props => {
-  const products = props.products;
-  const parentProduct = products.filter(product => {
-    return product.ParentCategoryId === 0;
-  });
-  console.log(parentProduct);
-  const hasSearch = !!props.searchText;
+class Product extends Component {
+  state = {};
 
-  // childRender(){
+  render() {
+    const products = this.props.products;
+    const hasSearch = !!this.props.searchText;
+    const parentProduct = products.filter(product => {
+      return product.ParentCategoryId === 0;
+    });
+    console.log(parentProduct);
+    console.log(products);
 
-  // }
-
-  return (
-    <div>
-      {parentProduct.map(parent => {
-        return (
-          <div key={parent.Id}>
-            <p className="list-item__title textHighlight list-item">
-              {parent.Name}
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+    return (
+      <div>
+        {parentProduct.map(parent => {
+          return (
+            <div key={parent.Id}>
+              {/* Parent Catagory Rendering Here */}
+              {!hasSearch && (
+                <p className="list-item__title textHighlight list-item">
+                  {parent.Name}
+                </p>
+              )}
+              {/* Child Catagory Rendering Here for every parent if needed*/}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default Product;
+
+// //fetching all the childs for a parent catagory
+// getChild = id => {
+//   const children = this.props.products.filter(child => {
+//     return child.ParentCategoryId === id;
+//   });
+//   return children;
+// };
+
+// {
+//   this.getChild(parent.Id).map(child => {
+//     return (
+//       <p
+//         key={child.Id}
+//         className="child-item__title textHighlight child-item"
+//       >
+//         {child.Name}
+//       </p>
+//     );
+//   })
+// }
